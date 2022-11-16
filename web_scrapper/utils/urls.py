5b6b391509bdc36489d7a15b11f7d4d6ae3d4c43
IdanMenaged <102684625+IdanMenaged@users.noreply.github.com>
2022-11-16 11:23:26 +0200

    web scrapper in progress

---
+++ web_scrapper/utils/urls.py
@@ -0,0 +1,20 @@
+from requests_html import HTMLSession, HTMLResponse
+import requests
+
+def get_source(url: str) -> HTMLResponse:
+    """Return the source code for the provided URL. 
+
+    Args: 
+        url (string): URL of the page to scrape.
+
+    Returns:
+        response (object): HTTP response object from requests_html. 
+    """
+
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
+
+    except requests.exceptions.RequestException as e:
+        raise e
