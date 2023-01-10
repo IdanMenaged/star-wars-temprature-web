ca5053d15a1a84315cd081f8b9e0a5583c349a4e
IdanMenaged <102684625+IdanMenaged@users.noreply.github.com>
2023-01-10 08:37:54 +0200

    weather as interface rather than class

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,24 @@
-class Weather {
-    minTemp: Number;
-    maxTemp: Number;
-    pressure: Number;
-    humidity: Number;
+// class Weather {
+//     minTemp: Number;
+//     maxTemp: Number;
+//     pressure: Number;
+//     humidity: Number;
     
-    constructor(minTemp: Number, maxTemp: Number, pressure: Number, humidity: Number) {
-        this.minTemp = minTemp;
-        this.maxTemp = maxTemp;
-        this.pressure = pressure;
-        this.humidity = humidity;
+//     constructor(minTemp: Number, maxTemp: Number, pressure: Number, humidity: Number) {
+//         this.minTemp = minTemp;
+//         this.maxTemp = maxTemp;
+//         this.pressure = pressure;
+//         this.humidity = humidity;
+//     }
+// }
 
-    }
+interface Weather {
+    minTemp: Number,
+    maxTemp: Number,
+    pressure: Number,
+    humidity: Number
 }
 
 const DescriptionToWeather: Array<[string, Weather]> = [
-    ['arid', new Weather()]
+    ['arid', { minTemp: -30, maxTemp: 130,  }]
 ]
