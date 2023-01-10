0d957e21df29f208c05c8d4e857f9d266b30e329
IdanMenaged <102684625+IdanMenaged@users.noreply.github.com>
2023-01-10 13:52:48 +0200

    translate temp to description

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,20 @@
-const DescriptionToTemp: Array<[string, Number]> = [
+const DescriptionToTemp: Array<[string, number]> = [
     ['arid', 30],
     ['temperate', 7.5],
     ['frozen', -11],
     ['murky', 20]
-]
+];
+
+export function getDescByTemp(targetTemp: number) {
+    const [initialDesc, initialTemp] = DescriptionToTemp[0];
+    let closestDiff: number = Math.abs(targetTemp - initialTemp);
+    let closestDesc: string = initialDesc;
+    DescriptionToTemp.forEach(([desc, temp]) => {
+        const difference = Math.abs(targetTemp - temp);
+        if (difference < closestDiff) {
+            closestDiff = difference;
+            closestDesc = desc;
+        }
+    });
+    return closestDesc;
+}
