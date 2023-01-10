2a6fcdc0f362afce97296c90a15d247ea83cc006
IdanMenaged <102684625+IdanMenaged@users.noreply.github.com>
2023-01-10 09:16:15 +0200

    description to temp table done

--- backend/index.ts
+++ backend/index.ts
@@ -1,24 +1,6 @@
-// class Weather {
-//     minTemp: Number;
-//     maxTemp: Number;
-//     pressure: Number;
-//     humidity: Number;
-    
-//     constructor(minTemp: Number, maxTemp: Number, pressure: Number, humidity: Number) {
-//         this.minTemp = minTemp;
-//         this.maxTemp = maxTemp;
-//         this.pressure = pressure;
-//         this.humidity = humidity;
-//     }
-// }
-
-interface Weather {
-    minTemp: Number,
-    maxTemp: Number,
-    pressure: Number,
-    humidity: Number
-}
-
-const DescriptionToWeather: Array<[string, Weather]> = [
-    ['arid', { minTemp: -30, maxTemp: 130,  }]
+const DescriptionToTemp: Array<[string, Number]> = [
+    ['arid', 30],
+    ['temperate', 7.5],
+    ['frozen', -11],
+    ['murky', 20]
 ]
